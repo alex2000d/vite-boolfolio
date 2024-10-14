582ac5fd8a6474493edf9685eaf3bcbf0e2687e6
alex2000d <demarcoalessandro777@gmail.com>
2024-10-14 20:02:24 +0200

    [UPD] print the all card projects

--- src/components/AppProject.vue
+++ src/components/AppProject.vue
@@ -12,7 +12,7 @@ export default {
     methods: {
         getProjects(){
             axios.get('http://127.0.0.1:8000/api/projects').then((results) =>{
-              console.log(results);
+              this.projects = results.data.results;
             });
         }
     },
@@ -23,7 +23,18 @@ export default {
         <div class="container">
             <div class="row">
                 <div class="col-12">
-                    <h1 class="text-center">Elenco dei progetti</h1>
+                    <h1 class="text-center my-5">Elenco dei progetti</h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-4" v-for="project in projects" :key="project.id">
+                    <div class="card">
+                        <img class="img-card-top" :src="project.image.startsWith('http') ? project.image : `http://127.0.0.1:8000/storage/${project.image}`" alt="Project Image">
+                        <div class="card-body">
+                            <h4 class="card-title">{{ project.title }}</h4>
+                            <p class="card-text">{{ project.description }}</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
