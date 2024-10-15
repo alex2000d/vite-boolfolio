ae86fef21f24723aae6bb79b4e1ee3111ba93fb9
alex2000d <demarcoalessandro777@gmail.com>
2024-10-15 15:36:39 +0200

    [UPD] created the new componets header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    
+}
+</script>
+<template>
+    <header>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <ul>
+                        <li>Home</li>
+                        <li>projects</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+<style lang="">
+    
+</style>
