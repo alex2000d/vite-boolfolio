00ce8e22a44a9181c345550c554b58713efc978b
alex2000d <demarcoalessandro777@gmail.com>
2024-10-16 16:34:24 +0200

    [UPD] created the single page for project

---
+++ src/pages/PageSingleProject.vue
@@ -0,0 +1,20 @@
+<script>
+import axios from 'axios';
+import { store } from '../store';
+export default {
+    data() {
+        return {
+            store,
+            project: null
+        }
+    },
+}
+</script>
+<template>
+    <div>
+        
+    </div>
+</template>
+<style lang="">
+    
+</style>
